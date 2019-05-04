d8b777c35e97b43e6b6ac9e8cd947aec35c44454
Rahul Aggarwal <rahaggar@adobe.com>
2019-05-04 18:38:05 +0530

    datatype json was incorrect. Removed that.

--- chat-bot/js/chat.js
+++ chat-bot/js/chat.js
@@ -1,9 +1,12 @@
 //client side script
+/**
+ * On click of send send post request to server. Datta on server is maintained in server side cache.
+ * Get is used to retrieve the data from server side cache and display on the screen.
+ */
 $(() => {
     $('#send').click(() => {
         var message = { name: $("#name").val(), message: $("#message").val() }
         postMessages(message);
-        getMessages();
     });
 
     getMessages();
@@ -12,7 +15,7 @@ $(() => {
 function getMessages() {
     $.get("http://localhost:3000/messages",
         (data, textStatus, jqXHR) => {
-            console.log(textStatus)
+            console.log('Inside success of getMessages : ', textStatus, data);
             addMessage(data)
         },
         "json"
@@ -25,15 +28,24 @@ function addMessage(data) {
         $('#messages').append(`<h1>${item.name}</h1> <p>${item.message}</p>`)
     });
 }
-
+/**
+ * @param {*} message 
+ * 
+ * data is always posted with 
+ * Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+ */
 function postMessages(message) {
     $.ajax({
         type: "post",
         url: "http://localhost:3000/messages",
         data: message,
-        dataType: "json",
-        success: function (response) {
-            console.log(response);
+
+        success: function (data, textStatus, jqXHR) {
+            console.log('Inside success of postMessages : ', textStatus, data);
+            getMessages();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('Inside ERROR of postMessages : ', textStatus, errorThrown);
         }
     });
 }
