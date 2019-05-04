36fb0346b38236ea12ce15cca939aa77b206f02a
Rahul Aggarwal <rahaggar@adobe.com>
2019-05-04 19:00:49 +0530

    bodyParser.urlencoded to support URL-encoded bodies true or false both work.

--- chat-bot/server.js
+++ chat-bot/server.js
@@ -6,8 +6,8 @@ const app = express()
 
 app.use(express.static(__dirname))
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies true or false both work.
+  extended: false
 })); 
 
 const messages = [
